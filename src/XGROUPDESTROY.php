6d16bbd22ad8b2a8e9dcd705f7783626f5119dc9
trgino <trgino@trgino.com>
2024-07-26 12:44:13 +0300

    Update XGROUPDESTROY.php

--- src/XGROUPDESTROY.php
+++ src/XGROUPDESTROY.php
@@ -17,38 +17,4 @@ class XGROUPDESTROY extends \Predis\Command\Command
 
         parent::setArguments($args);
     }
-
-    /**
-     * @param  array $arguments
-     * @return void
-     */
-    private function setCreateArguments(array $arguments): void
-    {
-        $processedArguments = [$arguments[0], $arguments[1], $arguments[2], $arguments[3]];
-
-        if (array_key_exists(4, $arguments) && true === $arguments[4]) {
-            $processedArguments[] = 'MKSTREAM';
-        }
-
-        if (array_key_exists(5, $arguments)) {
-            array_push($processedArguments, 'ENTRIESREAD', $arguments[5]);
-        }
-
-        parent::setArguments($processedArguments);
-    }
-
-    /**
-     * @param  array $arguments
-     * @return void
-     */
-    private function setSetIdArguments(array $arguments): void
-    {
-        $processedArguments = [$arguments[0], $arguments[1], $arguments[2], $arguments[3]];
-
-        if (array_key_exists(4, $arguments)) {
-            array_push($processedArguments, 'ENTRIESREAD', $arguments[4]);
-        }
-
-        parent::setArguments($processedArguments);
-    }
 }
